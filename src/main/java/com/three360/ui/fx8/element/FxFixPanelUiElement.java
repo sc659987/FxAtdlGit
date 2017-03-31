1dd1d4ddfd4ece13189b7ff92141950f3a86cd16
sainikchattapadhyay@gmail.com <Sainik123$>
2017-03-31 16:44:31 +0200

    minor bug solved

--- src/main/java/com/three360/ui/fx8/element/FxFixPanelUiElement.java
+++ src/main/java/com/three360/ui/fx8/element/FxFixPanelUiElement.java
@@ -20,148 +20,149 @@ import java.util.stream.Collectors;
 
 public class FxFixPanelUiElement implements IFixPanelUiElement<Node, EventHandler<ActionEvent>> {
 
-	private StrategyPanelT strategyPanelT;
+    private StrategyPanelT strategyPanelT;
 
-	private List<Region> regionList;
-	ScrollPane scrollPane;
+    private List<Region> regionList;
+    ScrollPane scrollPane;
 
-	private List<ParameterT> parameterTList;
+    private List<ParameterT> parameterTList;
 
-	@Inject
-	UiElementAbstractFactory factory;
+    @Inject
+    UiElementAbstractFactory factory;
 
-	public FxFixPanelUiElement() {
-		DaggerMyComponent.builder().build().inject(this);
-	}
+    public FxFixPanelUiElement() {
+        DaggerMyComponent.builder().build().inject(this);
+    }
 
-	@Override
-	public Node create() {
-		if (this.strategyPanelT != null) {
-			TitledPane titledPane = new TitledPane();
-			titledPane.setCollapsible(this.strategyPanelT.isCollapsible());
-			titledPane.setExpanded(!this.strategyPanelT.isCollapsed());
-			// titledPane.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-			titledPane.setText(this.strategyPanelT.getTitle());
-			Pane parent = (this.strategyPanelT.getOrientation() == PanelOrientationT.HORIZONTAL)
-					? new HBox(20.0) : new VBox();
-			// Container for either groups of parameters or strategyPanels, but not both.
-			// I.e., a StrategyPanel will contain either all Control elements or all
-			// StrategyPanel elements.
-			if (this.strategyPanelT.getStrategyPanel() != null && this.strategyPanelT.getStrategyPanel().size() > 0) {
-				parent.getChildren().addAll(this.strategyPanelT
-						.getStrategyPanel()
-						.stream()
-						.map(eachStrategyPanelT -> {
-							final FxFixPanelUiElement element = new FxFixPanelUiElement();
-							element.setStrategyPanelT(eachStrategyPanelT);
-							return element.create();
-						}).collect(Collectors.toList()));
-			} else if (this.strategyPanelT.getControl() != null) {
-				regionList = this.strategyPanelT.getControl().stream().map(controlT -> {
-					if (controlT instanceof CheckBoxListT) {
-						IFixCheckBoxListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewCheckBoxList();
-						element.setCheckBoxListT((CheckBoxListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof CheckBoxT) {
-						IFixCheckBoxUiElement<CheckBox, EventHandler<ActionEvent>> element = factory.instantiateNewCheckBox();
-						element.setCheckBoxT((CheckBoxT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof ClockT) {
-						IFixClockUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewClock();
-						element.setClockT((ClockT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof DoubleSpinnerT) {
-						// TODO complete the double spinner
-					} else if (controlT instanceof DropDownListT) {
-						IFixDropDownListUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewDropDownList();
-						element.setDropDownList((DropDownListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof EditableDropDownListT) {
-						IFixEditableDropDownListUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewEditableDropDownList();
-						element.setEditableDropDownList((EditableDropDownListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof LabelT) {
-						IFixLabelUiElement<Label, EventHandler<ActionEvent>> element = factory.instantiateNewLabel();
-						element.setLabel((LabelT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof MultiSelectListT) {
-						IFixMultiSelectListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewMultiSelectList();
-						element.setMultiSelectList((MultiSelectListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof RadioButtonListT) {
-						IFixRadioButtonListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewRadioButtonList();
-						element.setRadioButtonListT((RadioButtonListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof RadioButtonT) {
-						IFixRadioButtonUiElement<RadioButton, EventHandler<ActionEvent>> element = factory.instantiateNewRadioButton();
-						element.setRadioButtonT((RadioButtonT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof SingleSelectListT) {
-						IFixSingleSelectListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewSingleSelectList();
-						element.setSingleSelectList((SingleSelectListT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof SingleSpinnerT) {
-						IFixSingleSpinnerUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewSingleSpinner();
-						element.setSingleSpinner((SingleSpinnerT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof SliderT) {
-						IFixSliderUiElement<Slider, EventHandler<ActionEvent>> element = factory.instantiateNewSlider();
-						element.setSlider((SliderT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof TextFieldT) {
-						IFixTextFieldUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewTextField();
-						element.setTextField((TextFieldT) controlT);
-						element.setParameters(findParameterByName(controlT.getParameterRef()));
-						return element.create();
-					} else if (controlT instanceof HiddenFieldT) {
+    @Override
+    public Node create() {
+        if (this.strategyPanelT != null) {
+            TitledPane titledPane = new TitledPane();
+            titledPane.setCollapsible(this.strategyPanelT.isCollapsible());
+            titledPane.setExpanded(!this.strategyPanelT.isCollapsed());
+            // titledPane.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            titledPane.setText(this.strategyPanelT.getTitle());
+            Pane parent = (this.strategyPanelT.getOrientation() == PanelOrientationT.HORIZONTAL)
+                    ? new HBox(20.0) : new VBox();
+            // Container for either groups of parameters or strategyPanels, but not both.
+            // I.e., a StrategyPanel will contain either all Control elements or all
+            // StrategyPanel elements.
+            if (this.strategyPanelT.getStrategyPanel() != null && this.strategyPanelT.getStrategyPanel().size() > 0) {
+                parent.getChildren().addAll(this.strategyPanelT
+                        .getStrategyPanel()
+                        .stream()
+                        .map(eachStrategyPanelT -> {
+                            final FxFixPanelUiElement element = new FxFixPanelUiElement();
+                            element.setStrategyPanelT(eachStrategyPanelT);
+                            return element.create();
+                        }).collect(Collectors.toList()));
+            } else if (this.strategyPanelT.getControl() != null) {
+                regionList = this.strategyPanelT.getControl().stream().map(controlT -> {
+                    if (controlT instanceof CheckBoxListT) {
+                        IFixCheckBoxListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewCheckBoxList();
+                        element.setCheckBoxListT((CheckBoxListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof CheckBoxT) {
+                        IFixCheckBoxUiElement<CheckBox, EventHandler<ActionEvent>> element = factory.instantiateNewCheckBox();
+                        element.setCheckBoxT((CheckBoxT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof ClockT) {
+                        IFixClockUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewClock();
+                        element.setClockT((ClockT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof DoubleSpinnerT) {
+                        // TODO complete the double spinner
+                    } else if (controlT instanceof DropDownListT) {
+                        IFixDropDownListUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewDropDownList();
+                        element.setDropDownList((DropDownListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof EditableDropDownListT) {
+                        IFixEditableDropDownListUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewEditableDropDownList();
+                        element.setEditableDropDownList((EditableDropDownListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof LabelT) {
+                        IFixLabelUiElement<Label, EventHandler<ActionEvent>> element = factory.instantiateNewLabel();
+                        element.setLabel((LabelT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof MultiSelectListT) {
+                        IFixMultiSelectListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewMultiSelectList();
+                        element.setMultiSelectList((MultiSelectListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof RadioButtonListT) {
+                        IFixRadioButtonListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewRadioButtonList();
+                        element.setRadioButtonListT((RadioButtonListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof RadioButtonT) {
+                        IFixRadioButtonUiElement<RadioButton, EventHandler<ActionEvent>> element = factory.instantiateNewRadioButton();
+                        element.setRadioButtonT((RadioButtonT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof SingleSelectListT) {
+                        IFixSingleSelectListUiElement<Pane, EventHandler<ActionEvent>> element = factory.instantiateNewSingleSelectList();
+                        element.setSingleSelectList((SingleSelectListT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof SingleSpinnerT) {
+                        IFixSingleSpinnerUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewSingleSpinner();
+                        element.setSingleSpinner((SingleSpinnerT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof SliderT) {
+                        IFixSliderUiElement<Slider, EventHandler<ActionEvent>> element = factory.instantiateNewSlider();
+                        element.setSlider((SliderT) controlT);
+                        element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof TextFieldT) {
+                        IFixTextFieldUiElement<HBox, EventHandler<ActionEvent>> element = factory.instantiateNewTextField();
+                        element.setTextField((TextFieldT) controlT);
+                        if (controlT.getParameterRef() != null)
+                            element.setParameters(findParameterByName(controlT.getParameterRef()));
+                        return element.create();
+                    } else if (controlT instanceof HiddenFieldT) {
 
-					}
-					throw new RuntimeException("control is not recognized");
-				}).collect(Collectors.toList());
-				parent.getChildren().setAll(regionList);
-				scrollPane = new ScrollPane();
-				scrollPane.setContent(parent);
-			}
-			titledPane.setContent(parent);
-			return titledPane;
-		}
-		return null;
-	}
+                    }
+                    throw new RuntimeException("control is not recognized");
+                }).collect(Collectors.toList());
+                parent.getChildren().setAll(regionList);
+                scrollPane = new ScrollPane();
+                scrollPane.setContent(parent);
+            }
+            titledPane.setContent(parent);
+            return titledPane;
+        }
+        return null;
+    }
 
-	private Node createControlByType(ControlT controlT) {
+    private Node createControlByType(ControlT controlT) {
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public void registerForEvent(EventHandler<ActionEvent> e) {
+    @Override
+    public void registerForEvent(EventHandler<ActionEvent> e) {
 
-	}
+    }
 
-	@Override
-	public void setStrategyPanelT(StrategyPanelT strategyPanelT) {
-		this.strategyPanelT = strategyPanelT;
-	}
+    @Override
+    public void setStrategyPanelT(StrategyPanelT strategyPanelT) {
+        this.strategyPanelT = strategyPanelT;
+    }
 
-	@Override
-	public void setParameters(List<ParameterT> parameterTList) {
-		this.parameterTList = parameterTList;
-	}
+    @Override
+    public void setParameters(List<ParameterT> parameterTList) {
+        this.parameterTList = parameterTList;
+    }
 
-	@Override
-	public List<ParameterT> getParameter() {
-		return this.parameterTList;
-	}
+    @Override
+    public List<ParameterT> getParameter() {
+        return this.parameterTList;
+    }
 }
--- src/main/java/com/three360/ui/fx8/element/FxFixTextFieldUiElement.java
+++ src/main/java/com/three360/ui/fx8/element/FxFixTextFieldUiElement.java
@@ -1,6 +1,7 @@
 package com.three360.ui.fx8.element;
 
 import com.sun.deploy.util.StringUtils;
+import com.sun.istack.internal.NotNull;
 import com.three360.fixatdl.core.ParameterT;
 import com.three360.fixatdl.layout.TextFieldT;
 import com.three360.ui.Utils;
@@ -22,69 +23,70 @@ import java.util.List;
  */
 public class FxFixTextFieldUiElement implements IFixTextFieldUiElement<Pane, ChangeListener<String>> {
 
-	/***
-	 *
-	 */
-	private TextFieldT textFieldT;
+    /***
+     *
+     */
+    private TextFieldT textFieldT;
 
-	/****
-	 *
-	 */
-	private TextField textField;
-	private Label label;
-	private GridPane gridPane;
+    /****
+     *
+     */
+    private TextField textField;
+    private Label label;
+    private GridPane gridPane;
 
-	/****
-	 *
-	 */
-	private List<ParameterT> parameterTList;
+    /****
+     *
+     */
+    private List<ParameterT> parameterTList;
 
-	private ParameterT parameterT;
+    private ParameterT parameterT;
 
-	/****
-	 *
-	 */
-	private int nextColumn = 0;
+    /****
+     *
+     */
+    private int nextColumn = 0;
 
-	@Override
-	public Pane create() {
-		if (this.textFieldT != null) {
-			this.gridPane = new GridPane();
-			if (this.textFieldT.getLabel() != null && !this.textFieldT.getLabel().equals("")) {
-				this.gridPane.getColumnConstraints().addAll(FxUtils.getTwoColumnSameWidthForGridPane());
-				this.gridPane.add(this.label = new Label(this.textFieldT.getLabel()), this.nextColumn++, 0);
-			}
-			this.textField = new TextField(this.textFieldT.getInitValue());
-			this.textField.textProperty().addListener((observable, oldValue, newValue) -> {
-				if (newValue.equals("-"))
-					return;
-				if (!Utils.isEmpty(newValue) && !setFieldValueToParameter(newValue, parameterT))
-					textField.textProperty().setValue(oldValue);
-			});
-			this.gridPane.add(this.textField, this.nextColumn, 0);
-			return this.gridPane;
-		}
-		return null;
-	}
+    @Override
+    public Pane create() {
+        if (this.textFieldT != null) {
+            this.gridPane = new GridPane();
+            if (this.textFieldT.getLabel() != null && !this.textFieldT.getLabel().equals("")) {
+                this.gridPane.getColumnConstraints().addAll(FxUtils.getTwoColumnSameWidthForGridPane());
+                this.gridPane.add(this.label = new Label(this.textFieldT.getLabel()), this.nextColumn++, 0);
+            }
+            this.textField = new TextField(this.textFieldT.getInitValue());
+            this.textField.textProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue.equals("-") || parameterT == null)
+                    return;
+                if (!Utils.isEmpty(newValue) && !setFieldValueToParameter(newValue, parameterT))
+                    textField.textProperty().setValue(oldValue);
+            });
+            this.gridPane.add(this.textField, this.nextColumn, 0);
+            return this.gridPane;
+        }
+        return null;
+    }
 
-	@Override
-	public void setTextField(TextFieldT textField) {
-		this.textFieldT = textField;
-	}
+    @Override
+    public void setTextField(TextFieldT textField) {
+        this.textFieldT = textField;
+    }
 
-	@Override
-	public void registerForEvent(ChangeListener<String> e) {
+    @Override
+    public void registerForEvent(ChangeListener<String> e) {
 
-	}
+    }
 
-	@Override
-	public void setParameters(List<ParameterT> parameterTList) {
-		this.parameterTList = parameterTList;
-		// this.parameterT = this.parameterTList.get(0);
-	}
+    @Override
+    public void setParameters(@NotNull List<ParameterT> parameterTList) {
+        assert (parameterTList != null);
+        this.parameterTList = parameterTList;
+        this.parameterT = this.parameterTList.get(0);
+    }
 
-	@Override
-	public List<ParameterT> getParameter() {
-		return this.parameterTList;
-	}
+    @Override
+    public List<ParameterT> getParameter() {
+        return this.parameterTList;
+    }
 }
